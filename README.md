9d9d28b770c8db1d8d0dd69d479dc68f004514d0
karanba <altaykarakus@gmail..com>
2019-02-23 02:10:01 +0300

    Add command and menu settings

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # VSCode-TortiesHg-Commands
+
+## Overview
+
+This extension has shortcuts to be able to run some TortiesHG command for mercurial repos.
+
+# Prerequisites
+
+TortiesHG must be installed in your local and thg.exe must be in your path.
+
+![Hg](images/editor-context-menu.png) 
+
+# Features
+Following commands are avalible in explorer/context, editor/context and editor/title/context menus.
+ 
+* Annotate File Command
+* Revision History Command
+
+# Feedback & Contributing
+
+ * Please report any bugs, suggestions or documentation requests via the [Github issues](https://github.com/karanba/VSCode-TortiesHg-Commands/issues).
+ * Feel free to submit [pull requests](https://github.com/karanba/VSCode-TortiesHg-Commands/pulls).
---
+++ extension.js
@@ -0,0 +1,60 @@
+'use strict';
+const vscode = require('vscode');
+
+
+const runCommand = (uri, command) => {
+	let fileFullPath = undefined;
+
+	if (uri) {
+		fileFullPath = uri.fsPath;
+	} else if (vscode.window.activeTextEditor.document) {
+		fileFullPath = vscode.window.activeTextEditor.document.uri.fsPath;
+	}
+
+
+	if (fileFullPath) {
+		var activeTerminal = vscode.window.activeTerminal;
+		if (activeTerminal) {
+			activeTerminal.sendText(command + " " + fileFullPath);
+		} else {
+			vscode.window.showInformationMessage("To be able to run command, there must be an active terminal!");
+		}
+	} else {
+		vscode.window.showInformationMessage("Could not locate file");
+	}
+};
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "TortiesHg Commands" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	let revisionHistoryDisposable = vscode.commands.registerCommand('extension.revisionHistory', (uri) => {
+
+		runCommand(uri, "thg file");
+	});
+
+	context.subscriptions.push(revisionHistoryDisposable);
+
+	let annotateFileDisposable = vscode.commands.registerCommand('extension.annotateFile', (uri) => {
+		runCommand(uri, "thg annotate");
+	});
+
+	context.subscriptions.push(annotateFileDisposable);
+}
+
+
+// this method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
